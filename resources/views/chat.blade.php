63108555540d480fd72a9513354905f43fa65940
AlexVesely <veselyalex11@gmail.com>
2026-02-04 18:04:52 +0000

    Added button for 'Feedback Form', Implementation will come later

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -40,5 +40,12 @@
         </div>
     @endif
 
+    <div class="mt-4 text-center">
+    <p style="font-weight: bold;">Finished your session?</p>
+    <button>
+        Feedback Form (1-min Survey)
+    </button>
+    </div>
+
 </body>
 </html>
